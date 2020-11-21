7377aee34cb5a873e5b95276593d2f0599287b2b
ivanwinata96 <ivanwinata96@users.noreply.github.com>
2020-11-21 19:07:18 +0700

    Initial Commit

---
+++ Besepeda/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Besepeda
+//
+//  Created by Ivan Winata on 21/11/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
